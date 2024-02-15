25acd81b1835114beefea731ab749115c16dfbf7
Vipershroom <undertalefan812@gmail.com>
2024-02-15 13:58:45 -0500

    Add dinner party class

---
+++ src/DinnerParty.java
@@ -0,0 +1,12 @@
+public class DinnerParty {
+    private int dinnerMenu;
+    public int getDinnerMenu(){
+        return dinnerMenu;
+    }
+    public void setGuests(int a){
+        dinnerMenu = a;
+    }
+    public static void main(String[] args) {
+
+    }
+}
