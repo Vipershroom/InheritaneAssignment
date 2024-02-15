53e6953a8ca82df315bbde9b4c0fdc27df7c7db6
Vipershroom <undertalefan812@gmail.com>
2024-02-15 14:37:56 -0500

    Modify dinner party to add new getters and setters

--- src/DinnerParty.java
+++ src/DinnerParty.java
@@ -1,9 +1,16 @@
 public class DinnerParty {
     private int dinnerMenu;
+    private int dinnnerChoice;
     public int getDinnerMenu(){
         return dinnerMenu;
     }
     public void setGuests(int a){
         dinnerMenu = a;
+    }
+    public int getDinnerChoice(){
+        return dinnnerChoice;
+    }
+    public void setDinnerChoice(int a){
+        dinnnerChoice = a;
     }
 }
