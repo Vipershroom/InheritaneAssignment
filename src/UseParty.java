91d954984e1067b34d9e1ba6ee40eb429f514c18
Vipershroom <undertalefan812@gmail.com>
2024-02-18 15:15:46 -0500

    Added Code comments and edited prompt messages

--- src/UseParty.java
+++ src/UseParty.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 public class UseParty {
     public static void main(String[] args){
+
+        // Guests Portion
         int guests = 0;
         Party a = new Party();
         HashMap<Integer, String> menuItems = generate_menu();
@@ -13,29 +15,33 @@ public class UseParty {
         System.out.println("the party has "+guests+" guests");
         a.setGuests(guests);
         a.displayInvitation();
+
+        // Dinner Portion
         DinnerParty dinner = new DinnerParty();
-        System.out.println("How many guests are coming?");
+        System.out.println("How many guests are coming for the dinner party?");
         guests = s.nextInt();
         dinner.setGuests(guests);
-        System.out.println("the party has "+guests+" guests");
         
+        // Renders the menu by iterating through a hashmap
+        System.out.println("Enter the Menu Option");
         for (Map.Entry<Integer, String> i : menuItems.entrySet()) {
             System.out.println(i.getKey() + " " + i.getValue());
         }
-        System.out.println("Please enter your dinner choice");
-        Scanner num = new Scanner(System.in);
-        int choice = num.nextInt();
-        dinner.setDinnerChoice(choice); 
         
+        Scanner d = new Scanner(System.in);
+        int choice = d.nextInt();
+        dinner.setDinnerChoice(choice); 
+        System.out.println("the dinner party has "+guests+" guests");
+        System.out.println("Menu option " + choice + " will be served");
         dinner.displayInvitation();
     }
 
     public static HashMap<Integer, String> generate_menu() {
+        // Method that generates the hashmap. 
+        // The code was made into a method to make the code much cleaner
         HashMap<Integer, String> hash = new HashMap<Integer, String>();
         hash.put(1, "Chicken");
         hash.put(2, "Vegi");
-        hash.put(3, "Pasta");
-        hash.put(4, "Pad Thai");
         return hash;
     }
 }
