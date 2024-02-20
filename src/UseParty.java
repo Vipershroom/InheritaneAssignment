a565ca3ae7bf923e72cf134f4034f08726a71932
froncakes <75559235+froncakes@users.noreply.github.com>
2024-02-19 19:48:37 -0500

    Add dinner party class

--- src/UseParty.java
+++ src/UseParty.java
@@ -7,11 +7,11 @@ public class UseParty {
 
         // Guests Portion
         int guests = 0;
-        Party a = new Party();
         HashMap<Integer, String> menuItems = generate_menu();
+        Party a = new Party();
         Scanner s = new Scanner(System.in);
         System.out.println("How many guests are coming?");
-        guests = s.nextInt();
+        guests = inputNumber();
         System.out.println("the party has "+guests+" guests");
         a.setGuests(guests);
         a.displayInvitation();
@@ -19,21 +19,33 @@ public class UseParty {
         // Dinner Portion
         DinnerParty dinner = new DinnerParty();
         System.out.println("How many guests are coming for the dinner party?");
-        guests = s.nextInt();
+        guests = inputNumber();
         dinner.setGuests(guests);
-        
+
         // Renders the menu by iterating through a hashmap
         System.out.println("Enter the Menu Option");
         for (Map.Entry<Integer, String> i : menuItems.entrySet()) {
             System.out.println(i.getKey() + " " + i.getValue());
         }
-        
-        Scanner d = new Scanner(System.in);
-        int choice = d.nextInt();
+
+        int choice = inputNumber();
         dinner.setDinnerChoice(choice); 
         System.out.println("the dinner party has "+guests+" guests");
         System.out.println("Menu option " + choice + " will be served");
         dinner.displayInvitation();
+    }
+
+    public static int inputNumber(){
+        // checks if next line inputted is an int, if not loops through the choice again.
+        System.out.println("Please input a number");
+        Scanner d = new Scanner(System.in);
+        int c = 0;
+        if (d.hasNextInt()){
+            c = d.nextInt();
+        }else {
+            c = inputNumber();
+        }
+        return (c);
     }
 
     public static HashMap<Integer, String> generate_menu() {
